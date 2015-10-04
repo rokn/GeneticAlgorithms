4523afde2fd056e91443f3e691b44b95167bf7b9
Antonio <amindov@abv.bg>
2015-10-04 16:20:21 +0300

    Added Fitness Functions (still not working)

--- ExpressionFinder/genetics.cpp
+++ ExpressionFinder/genetics.cpp
@@ -1,60 +1,83 @@
 #include "genetics.h"
 
-#define RANDOM_NUM	((float)rand()/(RAND_MAX+1))
+#define RANDOM_NUM	((float)rand()/(RAND_MAX+1) + 1)
 
-chromosome_t roulette_selection(chromosome_t *population, int pop_size)
+std::string get_random_chromo(int length)
 {
-	float total_fitness = 0;
+	std::string genes;
 
 	int i;
 	
-	for (i = 0; i < pop_size; ++i)
+	for (i = 0; i < length; ++i)
 	{
-		total_fitness += population[i].get_fitness();
+		if(RANDOM_NUM >= 0.5)
+		{
+			genes.push_back('0');
+		}
+		else
+		{
+			genes.push_back('1');
+		}
+	}
+
+	return genes;
+}
+
+std::string roulette_selection(std::vector<chromosome_t> *population, int pop_size)
+{
+	float total_fitness = 0;
+
+	std::vector<chromosome_t>::iterator i;
+	
+	for (i = population->begin(); i != population->end(); ++i)
+	{
+		total_fitness += (*i).get_fitness();
 	}
 
 	float random_fitness = RANDOM_NUM * total_fitness;
 
 	float curr_fitness = 0;
 	
-	for (i = 0; i < pop_size; ++i)
+	for (i = population->begin(); i != population->end(); ++i)
 	{
-		curr_fitness += population[i].get_fitness();
+		curr_fitness += (*i).get_fitness();
 
 		if(curr_fitness >= random_fitness)
 		{
-			return population[i];
+			return (*i).get_genes();
 		}
 	}
+
+	return NULL;
 }
 
-void crossover(float rate, std::string &offspring1, std::string &offspring1)
+void crossover(float rate, std::string *offspring1, std::string *offspring2)
 {
 	if(rate >= RANDOM_NUM)
 	{
-		int random_bit = (int) (RANDOM_NUM * offspring1.length());
+		int random_bit = (int) (RANDOM_NUM * offspring1->length());
 
-		std::string tmp = offspring1.substr(random_bit, offspring1.length() - random_bit);
-		offspring1.replace(random_bit, offspring1.length() - random_bit, offspring2, random_bit, offspring2.length() - random_bit);
-		offspring2.replace(random_bit, offspring2.length() - random_bit, tmp);
+		std::string tmp = offspring1->substr(random_bit, offspring1->length() - random_bit);
+		offspring1->replace(random_bit, offspring1->length() - random_bit, *offspring2, random_bit, offspring2->length() - random_bit);
+		offspring2->replace(random_bit, offspring2->length() - random_bit, tmp);
 	}
 }
 
-void mutate(float rate, std::string &genes)
+void mutate(float rate, std::string *genes)
 {
 	int i;
-	
-	for (i = 0; i < genes.length; ++i)
+	for (i = 0; i < genes->length(); ++i)
 	{
-		if(rate >= RANDOM_NUM)
+		float r = RANDOM_NUM;
+		if(rate >= r)
 		{
-			if(genes[i] == '0')
+			if(genes->at(i) == '0')
 			{
-				genes[i] = '1';
+				genes->replace(i, 1, "1");
 			}
 			else
 			{
-				genes[i] = '0';
+				genes->replace(i, 1, "0");
 			}
 		}
 	}
--- ExpressionFinder/main.cpp
+++ ExpressionFinder/main.cpp
@@ -1,9 +1,161 @@
 #include <iostream>
+#include <vector>
 #include "chromo.h"
+#include "genetics.h"
+#include <time.h>
+#include <stdlib.h>
+#include <cmath>
+
+#define POPULATION_SIZE 100
+#define GENES_COUNT 5
+#define GENE_LENGTH 4
+#define CROSSOVER_RATE 0.7f
+#define MUTATION_RATE 0.01f
+
+using namespace std;
+
+float CalculateFitness(string, float);
+vector<int> DecodeGenes(string);
+int BinToDec(string);
 
 int main()
 {
-	chromosome_t chromosome("10101010", 50);
-	std::cout << chromosome.get_genes();	
+	srand(time(NULL));
+	vector<chromosome_t> population;
+	vector<chromosome_t> tmp;
+	bool end = false;
+	float target;
+
+	cin >> target;
+
+	int i;
+	
+	for (i = 0; i < POPULATION_SIZE; ++i)
+	{
+		chromosome_t chromo(get_random_chromo(GENES_COUNT * GENE_LENGTH), 0);
+		population.push_back(chromo);
+	}
+
+	while(!end)
+	{
+
+		tmp.clear();
+		vector<chromosome_t>::iterator it;
+
+		for (it = population.begin(); it != population.end(); ++it)
+		{
+
+			it->set_fitness(CalculateFitness(it->get_genes(), target));
+
+			if(it->get_fitness() > 100.0f)
+			{
+				end = true;
+			}
+		}
+
+		for (i = 0; i < POPULATION_SIZE; ++i)
+		{
+			string offspring1 = roulette_selection(&population, POPULATION_SIZE);
+			string offspring2 = roulette_selection(&population, POPULATION_SIZE);
+			
+			crossover(CROSSOVER_RATE, &offspring1, &offspring2);
+
+			mutate(MUTATION_RATE, &offspring1);
+			mutate(MUTATION_RATE, &offspring2);
+
+			chromosome_t chromo1(offspring1, 0);
+			chromosome_t chromo2(offspring2, 0);
+
+			tmp.push_back(chromo1);
+			tmp.push_back(chromo2);
+		}		
+
+		population = tmp;
+
+		
+	}
+
 	return 0;
+}
+
+float CalculateFitness(string genes, float target)
+{
+	float fitness;
+
+	vector<int> decoded = DecodeGenes(genes);
+
+	vector<int>::iterator i;	
+
+	for (i = decoded.begin(); i != decoded.end(); ++i)
+	{
+		switch(*i)
+		{
+			case 10:
+				fitness += decoded[*(++i)];
+				break;
+
+			case 11:
+				fitness -= decoded[*(++i)];
+				break;
+
+			case 12:
+				fitness *= decoded[*(++i)];
+				break;
+
+			case 13:
+				fitness /= decoded[*(++i)];
+				break;
+		}
+	}	
+
+	if(fitness == target)
+	{
+		return 1000.0f;
+	}
+	else
+	{
+		fitness = 1 / (target - fitness);
+	}
+
+	return fitness;
+}
+
+vector<int> DecodeGenes(string genes)
+{
+	vector<int> decoded;
+	bool operTime = true;
+	int i;
+	int curr_gene;
+
+	for (i = 0; i < GENES_COUNT - 1; ++i)
+	{
+		curr_gene = BinToDec(genes.substr(i * GENE_LENGTH, GENE_LENGTH));
+
+		if(operTime && curr_gene >= 10 && curr_gene < 14)
+		{
+			decoded.push_back(curr_gene);
+		}
+		else if(!operTime && curr_gene < 10)
+		{
+			decoded.push_back(curr_gene);
+		}
+	}
+
+	return decoded;
+}
+
+int BinToDec(string binary)
+{
+	int numb = 0;
+	int i;
+	
+	for (i = binary.length() - 1; i >= 0; --i)
+	{
+		if(binary[i] == '1')
+		{
+			numb += pow(2, (binary.length() - 1) - i);
+		}
+	}
+
+	return numb;
 }
